8a9d338138a68e4938f338903a7ea809508884c5
Mari-Artho <mari.artho@medieinstitutet.se>
2022-06-07 20:58:26 +0200

    Connect to SQL DB.

--- app.js
+++ app.js
@@ -2,16 +2,27 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const mysql = require('mysql2');
+const cors = require('cors');
 
 var app = express();
+
+//Connect to mySQL(phpMyAdmin)
+app.locals.con = mysql.createConnection({
+    host: "localhost",
+    port: "8889",
+    user: "usernote",
+    password: "usernote",
+    database: "usernote"
+});
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
+const mysql = require('mysql2');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+req.app.locals.con.connect(function(err){
+  if(err){
+    console.log(err)
+  }
+  let rubrik = "Project T";
+  let text = "Cultivating new customers.";
+  let employeeName = "Obama Carlsson";
+  //Send data to database
+  // let sql = `
+  //   INSERT INTO note( rubrik, text, employeeName) 
+  //   VALUES("${rubrik}","${text}", "${employeeName}")
+  // `
+
+  let sql = ` SELECT * FROM note `
+
+
+  req.app.locals.con.query(sql, function(err, result){
+    if(err){
+      console.log(err);
+    }
+    console.log("result", result);
+    res.send(result);
+  })
+})
+  //res.send('respond with a resource');
 });
 
 module.exports = router;
